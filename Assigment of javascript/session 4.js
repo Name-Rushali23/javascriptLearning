1318b2d13a187a60ab156dd0ba14cf784abdf9dd
Name-Rushali23 <khalkarrushali@gmail.com>
2022-11-03 13:25:30 +0530

    session4 assignment
    session4 assignment

---
+++ Assigment of javascript/session 4.js
@@ -0,0 +1,43 @@
+//squer of number//
+
+function squer(arg1){
+  var squer=arg1*arg1;
+    console.log(squer);
+}
+squer(5);
+squer(6);
+squer(25);
+squer(100);
+
+//type of function//
+
+function type(){
+    console.log(typeof type);
+
+}
+type();
+
+//area of traingal//
+
+var Area=function(base,height){
+    var Ans=1/2*base*height;
+    console.log("Area=" ,Ans);
+}
+var result=Area(45,34);
+
+//chek positive negative number
+
+function check(Num){
+    if(Num>0)
+    {
+        console.log("positive");
+    }
+    else{
+        console.log("Negative");
+    }
+    }
+    check(54);
+    check(0.567);
+    check(890);
+    check(00);
+
