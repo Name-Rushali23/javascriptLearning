c2df2dcab5cf55be0205c1c1416f90fb8372ab80
Name-Rushali23 <khalkarrushali@gmail.com>
2022-11-10 20:12:54 +0530

    session4 completed
    session4 completed

---
+++ Assigment of javascript/session21.js
@@ -0,0 +1,10 @@
+console.log("===== Traversing map using forEach() =====");
+let map = new Map();map.set(22, "Sachin");
+map.set(11, "Dravid");map.set(33, "Gaungully");
+map.set(77, "Dravid");
+map.forEach( (key, value) => {console.log(key, value);} );
+
+console.log("====== reduce()=====");
+let sum = arrayOfNumbers.reduce( (value, runningTotal) => {return value + runningTotal;} );
+console.log(sum);
+
